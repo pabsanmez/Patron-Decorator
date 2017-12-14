052b8695972e42aed6b545b929dfc282b4145186
pabsanmez <pabsanmez@gmail.com>
2017-12-14 19:59:38 +0100

    Initial Commit

---
+++ Patron-DecoratorTests/Patron_DecoratorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Patron_DecoratorTests.swift
+//  Patron-DecoratorTests
+//
+//  Created by Pablo Sánchez Gómez on 14/12/17.
+//  Copyright © 2017 Pablo Sánchez Gómez. All rights reserved.
+//
+
+import XCTest
+@testable import Patron_Decorator
+
+class Patron_DecoratorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
